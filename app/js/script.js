c521d6c48095202e8badacb758196c6ecf37995e
Cesar Funes <fuunesto@gmail.com>
2022-09-03 01:42:50 -0500

    :

--- app/js/script.js
+++ app/js/script.js
@@ -3,11 +3,13 @@ const navMobile = document.querySelector('.nav-mobile')
 const navIcon = document.querySelector('.nav-mobile__icon')
 const navLinks = document.querySelector('.nav-mobile__links')
 
+nav.removeAttribute('scrolling')
+
 setInterval(() => {
   const isScrolling = window.scrollY != 0
 
   if (isScrolling) {
-    navMobile.setAttribute("scrolling", "")
+    navMobile.setAttribute('scrolling', '')
     return nav.setAttribute('scrolling', '')
   }
 
